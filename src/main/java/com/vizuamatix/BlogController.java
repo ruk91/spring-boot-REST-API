35cbbef10fc26fb07e1f68d2c39c1801b1ef45e1
Sajith Wijerathne <sajith@vizuamatix.com>
2019-02-22 15:36:14 +0530

    Added comments

--- src/main/java/com/vizuamatix/BlogController.java
+++ src/main/java/com/vizuamatix/BlogController.java
@@ -1,8 +1,10 @@
 package com.vizuamatix;
 
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//  @RestController annotation tells that this class is a controller
 @RestController
 public class BlogController {
 
--- src/main/java/com/vizuamatix/MainApplicationClass.java
+++ src/main/java/com/vizuamatix/MainApplicationClass.java
@@ -1,9 +1,12 @@
 package com.vizuamatix;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// @SpringBootApplication annotation is used to class in order to m ake it a Spring Boot Application
 @SpringBootApplication
+
 public class MainApplicationClass {
 
     public static void main(String[] args) {
